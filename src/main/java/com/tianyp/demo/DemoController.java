d691d60ad40ae9d16373677092837c3a2e476745
tianyp <lilinshan521@qq.com>
2017-08-09 16:20:03 +0800

    message

---
+++ src/main/java/com/tianyp/demo/DemoController.java
@@ -0,0 +1,22 @@
+package com.tianyp.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 
+ * @author typ
+ *
+ */
+@RestController
+public class DemoController {
+    
+    @Value("${size}")
+    private int size;
+        
+    @RequestMapping(value="/holle",method = RequestMethod.GET)
+    public String say(){
+        return "holle  word"+size;
+    }
+}
